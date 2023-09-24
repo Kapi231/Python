c1663bdddf57c7e41aa433aa7d9358e3f4fd5436
Kapi231 <zola.kacper@gmail.com>
2023-09-24 19:51:00 +0300

    working on hash tool

--- Hash_tool/Hedgehog.py
+++ Hash_tool/Hedgehog.py
@@ -1,11 +1,15 @@
 
 import hashlib
 
-h = hashlib.sha256(b'Hello').hexdigest()
+targetHash = "1234"
+hashReady = bytes(targetHash.encode('utf-8'))
+h = hashlib.sha256(hashReady).hexdigest()
 
-print(h)
+file = open("passToHash.txt", "r")
 
-if h == 'Hello':
-    print('Yes')
-else:
-    print('No')
+for line in file.readlines():
+    h2 = hashlib.sha256(b"1234").hexdigest()
+    if h == h2:
+        print("yay")
+    else:
+        pass
