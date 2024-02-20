3927a944de30ddd95cd3bee9317f4b9f2d6c05e8
Kapi231 <Kap1_priv@proton.me>
2024-02-20 14:09:10 +0100

    client/server
    server - refactor
    client - command added

--- Chat_app/Server.py
+++ Chat_app/Server.py
@@ -2,23 +2,13 @@ import socket
 import sys
 from multiprocessing import Process
 
-server_ip = "127.0.0.1"
-server_port = 9998
-server_admin = "admin" # username privilaged to close server (line 21)
-
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 def main():
-    server.bind((server_ip, server_port))
-    print(f"[*] server on {server_ip}:{server_port}")
-
-def message_maintainer():
     # receive packets from users
     data, (addr, port) = server.recvfrom(4096)
     print(f"[*] packet from {addr}:{port} :: {data.decode()}")
     
     #checks if command is send from privilaged user, not good security...
-    if data.decode() == f"{server_admin}: ?exit" and addr == server_ip:
+    if data.decode() == f"{server_admin}: ?shutdown" and addr == server_ip:
         server.sendto(b'[*] Closing server\n[*] Disconnecting...', (addr, port))
         server.close()
         sys.exit()
@@ -27,13 +17,17 @@ def message_maintainer():
         # send text back
         server.sendto(data, (addr, port))
 
-def procceses():
-    p1 = Process(target=message_maintainer())
-
-    p1.start()
-
 if __name__ == '__main__':
-    main() 
+
+    print("<==========CHAT APP SEVER==========>\n")
+
+    server_ip = "127.0.0.1"
+    server_port = 9998
+    server_admin = "admin" # username privilaged to close server (line 21)
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((server_ip, server_port))
+    print(f"[*] server on {server_ip}:{server_port}")    
 
     while 1:
-        procceses()
+        main()
--- Chat_app/client.py
+++ Chat_app/client.py
@@ -1,8 +1,12 @@
 import socket, sys
 
+print("<==========CHAT APP==========>\n")
+help = " ? - admin command\n / - user command\n ?shutdown - close server\n /exit - exit app"
+print(help)
+
 server_addr = ("127.0.0.1", 9998)
 
-username = input("[*] how other users should call you? \n   --> ")
+username = input("\n[*] how other users should call you? \n   --> ")
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if username != "" and len(username) >= 4:
@@ -12,6 +16,18 @@ if username != "" and len(username) >= 4:
 else:
     print("[*] invalid username!")
     sys.exit()
+
+def switch(command):
+
+    if command == "/help":
+        print(help)
+
+    elif command == "/exit":
+        print("[*] Closing app")
+        sys.exit()
+
+    else:
+        pass
 
 def sending_and_reciving():
     # send message to server
