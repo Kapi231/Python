8d7454489d8c3bf3a3b70031774016ec546facad
Kapi231 <Kap1_priv@proton.me>
2024-02-20 16:26:36 +0100

    Chat live
    established a request for chat log

--- Chat_app/Server.py
+++ Chat_app/Server.py
@@ -6,10 +6,12 @@ def main():
     # receive packets from users
     data, (addr, port) = server.recvfrom(4096)
     print(f"[*] packet from {addr}:{port} :: {data.decode()}")
-    log_append(data.decode())
+
+    if data.decode() == "<data_request>":
+        server.sendto(send_log(), (addr, port))
 
     #checks if command is send from privilaged user, not good security...
-    if data.decode() == f"{server_admin}: ?shutdown" and addr == server_ip:
+    elif data.decode() == f"{server_admin}: ?shutdown" and addr == server_ip:
         server.sendto(b'[*] Closing server\n[*] Disconnecting...', (addr, port))
         server.close()
         sys.exit()
@@ -20,12 +22,20 @@ def main():
             server.sendto(b'[*] Chat log cleared', (addr, port))
  
     else:
-        # send text back
-        server.sendto(data, (addr, port))
+        # append log and send text back
+        if data.decode() != "<data_request>":
+            log_append(data.decode())
+
+        server.sendto(send_log(), (addr, port))
 
 def log_append(text):
     with open('chat_log.txt', 'a') as file:
         file.writelines(f"{text}\n")
+
+def send_log():
+    with open('chat_log.txt', 'r') as chat:
+        return chat.read().encode()
+        
 
 if __name__ == '__main__':
 
@@ -37,7 +47,7 @@ if __name__ == '__main__':
 
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind((server_ip, server_port))
-    print(f"[*] server on {server_ip}:{server_port}")    
+    print(f"[*] server on {server_ip}:{server_port}")
 
     while 1:
         main()
--- Chat_app/client.py
+++ Chat_app/client.py
@@ -1,49 +1,68 @@
-import socket, sys
+import getpass, os, socket, sys, time
+from threading import Thread
 
-print("<==========CHAT APP==========>\n")
-help = " ? - admin command\n / - user command\n ?shutdown - close server\n /exit - exit app"
-print(help)
+def log_request():
+    #requesting chat log to have live chat
+    while 1:
+        try:
+            time.sleep(3)
+            client.sendto("<data_request>".encode(), server_addr)
+            log, addr = client.recvfrom(4096)
+            os.system("cls")
+            print(log.decode())
 
-server_addr = ("127.0.0.1", 9998)
-
-username = input("\n[*] how other users should call you? \n   --> ")
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-if username != "" and len(username) >= 4:
-    print(f"[*] Hi {username}")
-    pass
-
-else:
-    print("[*] invalid username!")
-    sys.exit()
+        except:
+            print("[*] Lost connection with server")
+            break
 
 def switch(command):
 
-    if command == "/help":
-        print(help)
-
-    elif command == "/exit":
+    if command == "/exit":
         print("[*] Closing app")
         sys.exit()
 
     else:
-        pass
+        pass  
 
-def sending_and_reciving():
+def main():
     # send message to server
-    text = input(">>> ")
-
+    text = getpass.getpass("")
     if text == "/exit":
         sys.exit()
+
+    os.system("cls")
 
     try:
         client.sendto(f"{username}: {text}".encode(), server_addr)
         mess, addr = client.recvfrom(4096)
         print(mess.decode())
-    
+        
     except:
         print("[*] Can't send/recive package, server is down")
         sys.exit()
 
-while 1:
-    sending_and_reciving()
+if __name__ == '__main__':
+
+    print("<==========CHAT APP==========>\n")
+    print(" ? - admin command\n / - user command\n ?shutdown - close server\n /exit - exit app")
+    getpass.getpass("\n[*] Press any key to continue")
+
+    server_addr = ("127.0.0.1", 9998)
+
+    username = input("\n[*] how other users should call you? \n   --> ")
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    if username != "" and len(username) >= 4:
+        print(f"[*] Hi {username}")
+        pass
+
+    else:
+        print("[*] invalid username!")
+        sys.exit()
+
+    log_thread = Thread(target=log_request)
+    log_thread.daemon = True
+    log_thread.start()
+
+    while 1:
+        main()
